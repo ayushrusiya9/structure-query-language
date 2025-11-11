d1863c014287f770d5f4c985f5d54777087b9dfe
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-11-11 15:17:44 +0530

    update

---
+++ practice_partition_by_day2.sql
@@ -0,0 +1,42 @@
+use window_function;
+
+select *,
+sum(sales_amount) over(partition by sales_rep order by sales_amount desc) as month
+from sales_data; 
+
+select *,
+sum(sales_amount) over(partition by region) as avg_amount
+from sales_data; 
+
+select *,
+lag(sales_amount) over(partition by sales_rep order by sales_amount) as last_month
+from sales_data; 
+
+select *,
+lead(sales_amount) over(partition by sales_rep order by sales_amount) as last_month
+from sales_data; 
+
+
+select *,
+sum(sales_amount) over(partition by region) as total, 
+sales_amount * 100 / sum(sales_amount) over(partition by region) as total_sale_percentage
+from sales_data;
+
+select *,
+max(sales_AMOUNT) over(partition by month ) as total_sales
+from sales_data;
+
+select *,
+lag(sales_amount) over(partition by sales_rep order by month) as lag_amount,
+case when lag(sales_amount) over(partition by sales_rep order by month) > sales_amount then "increase"
+when lag(sales_amount) over(partition by sales_rep order by month) < sales_amount then "decrease"
+else "not change"
+end as lag_division
+from sales_data;
+
+select *,
+row_number() over(partition by month order by sales_amount) as row_num
+from sales_data;
+
+
+
