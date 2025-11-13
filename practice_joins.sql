83f8ee2b7458ea1b5b4370f2af00a9cc1f3fe68a
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-11-13 15:21:54 +0530

    update

---
+++ practice_joins.sql
@@ -0,0 +1,18 @@
+use dummy;
+
+select * from customers;
+
+select * from customers c inner join orders o
+on c.customernumber = o.customernumber;
+
+select * from customers natural join orders;
+
+select * from customers c left join orders o
+on c.customernumber = o.customernumber
+union
+select * from customers c right join orders o
+on c.customernumber = o.customernumber;
+
+select * from customers c left join orders o
+on c.customernumber = o.customernumber 
+where ordernumber is null;
